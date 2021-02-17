658dfcd3cae63e698036c0c445e2ee6ac6530bcb
Camilo Guzman <camiloge_91@hotmail.com>
2021-02-17 13:15:57 -0500

    lazy&props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useContext } from 'react'
+import React, { useContext, Suspense } from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { NavBar } from './components/NavBar'
 import { Detail } from './pages/Detail'
-import { Favs } from './pages/Favs'
+// import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { Notfound } from './pages/NotFound'
@@ -11,25 +11,27 @@ import { Home } from './pages/Home'
 import { Router, Redirect } from '@reach/router'
 import { Context } from './Context'
 
+const Favs = React.lazy(() => import('./pages/Favs')) 
+
 export const App = () => {
   const {isAuth} = useContext(Context)
   return (
-    <div>
-    <GlobalStyle />
-    <Logo />
-    <Router>
-      <Notfound default />
-      <Home path='/' />
-      <Home path='/pet/:categoryId' />
-      <Detail path='/detail/:detailId' />
-      {!isAuth && <NotRegisteredUser path='/login' />}
-      {!isAuth && <Redirect noThrow from='/favs' to='/login' />}
-      {!isAuth && <Redirect noThrow from='/user' to='/login' />}
-      {isAuth && <Redirect noThrow from='/login' to='/' />}
-      <Favs path='/favs' />
-      <User path='/user' />
-    </Router>
-    <NavBar />
-  </div>
+    <Suspense fallback={<div />}>
+      <GlobalStyle />
+      <Logo />
+      <Router>
+        <Notfound default />
+        <Home path='/' />
+        <Home path='/pet/:categoryId' />
+        <Detail path='/detail/:detailId' />
+        {!isAuth && <NotRegisteredUser path='/login' />}
+        {!isAuth && <Redirect noThrow from='/favs' to='/login' />}
+        {!isAuth && <Redirect noThrow from='/user' to='/login' />}
+        {isAuth && <Redirect noThrow from='/login' to='/' />}
+        <Favs path='/favs' />
+        <User path='/user' />
+      </Router>
+      <NavBar />
+    </Suspense>
   )
 }
--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { FavsWithQuery } from '../container/GetFavorites'
 import { Layout } from '../components/Layout'
 
-export const Favs = () => (
+export default () => (
   <Layout title='Tus favoritos' subtitle='Aquí se encuentran tus favoritos'>
     <FavsWithQuery />
   </Layout>
